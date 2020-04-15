d77fa7092308b0118984eb72990260c454ec0d54
Sunil Karki <karkisunil1200@gmail.com>
2020-04-15 16:36:12 -0500

    All CRUD functionality works

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,11 @@ const db = require('../data/dbConfig');
 
 module.exports = {
   find,
-  findById
+  findById,
+  add,
+  findSteps,
+  update,
+  remove
 };
 
 function find() {
@@ -15,4 +19,44 @@ function findById(id) {
     .from('schemes')
     .where({id})
     .first();
+}
+
+function add(data) {
+  return db
+    .select('*')
+    .from('schemes')
+    .insert(data, 'id')
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function findSteps(id) {
+  // not exactly what I wanted
+  return db
+    .select('*')
+    .from('schemes')
+    .where({id})
+    .then(() => {
+      return db.select('*').from('steps');
+    });
+}
+
+function update(changes, id) {
+  return db
+    .select('*')
+    .from('schemes')
+    .where({id})
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db
+    .select('*')
+    .from('schemes')
+    .where({id})
+    .del();
 }
