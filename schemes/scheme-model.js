ffaab491a4d27a24bc4469e693028f8ecfde7231
Sunil Karki <karkisunil1200@gmail.com>
2020-05-13 15:49:35 -0500

    Added add functionality

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,38 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  find
+  find,
+  findById,
+  findSteps,
+  add
 };
 
 function find() {
   return db.select('*').from('schemes');
+}
+
+function findById(id) {
+  return db
+    .select('*')
+    .from('schemes')
+    .where({id})
+    .first();
+}
+
+function findSteps(id) {
+  return db
+    .select('*')
+    .from('steps')
+    .join('schemes', 'steps.scheme_id', 'schemes.id');
+  // .where(`steps.scheme_id`, `scheme.${id}`);
+}
+
+function add(data) {
+  return db
+    .select('*')
+    .from('schemes')
+    .insert(data, 'id')
+    .then(([id]) => {
+      return findById(id);
+    });
 }
